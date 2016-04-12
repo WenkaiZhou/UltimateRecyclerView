cb38fcd59ab1ced6be379d51d8424bfff2c2b0c2
zhouwk <zwenkai@foxmail.com>
2016-04-12 15:58:46 +0800

    remove Toast

--- ultimaterecyclerview/src/main/java/com/kevin/ultimaterecyclerview/UltimateRecyclerView.java
+++ ultimaterecyclerview/src/main/java/com/kevin/ultimaterecyclerview/UltimateRecyclerView.java
@@ -11,7 +11,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.handmark.pulltorefresh.library.LoadingLayoutBase;
 import com.handmark.pulltorefresh.library.LoadingLayoutProxy;
